a7646173a6197f4a666f8cbd34ce2e800206546e
Ritz Punsalan <ritzpunsalan@gmail.com>
2020-01-13 03:17:01 -0800

    meep

--- Section 4 Files System and Command Line Args Notes app/17_apps.js
+++ Section 4 Files System and Command Line Args Notes app/17_apps.js
@@ -5,17 +5,60 @@ const getNotes = require('./15notes.js')
 // Customize yargs version
 yargs.version('1.1.0')
 
+// // Create add command
+// yargs.command({
+//     command: 'add',
+//     describe: 'Add a new note',
+//     builder: {
+//         title: {
+//             describe:'Note title',
+//             demandOption: true,
+//             type: 'string'
+//         }
+//     },
+//     handler: function (argv) {
+//         // console.log('Adding a new note!', argv)
+//         console.log('Title: ' + argv.title)
+//     }
+// })
+
 // Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
     builder: {
         title: {
-            describe:'Note title '
+            describe:'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler: function () {
-        console.log('Adding a new note!', argv)
+    handler: function (argv) {
+        // console.log('Adding a new note!', argv)
+        console.log('Title: ' + argv.title)
+    }
+})
+
+
+// Create add command
+yargs.command({
+    command: 'bloop',
+    describe: 'bleep bleep a new note',
+    builder: {
+        title: {
+            describe:'Fawk the note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        // console.log('Adding a new note!', argv)
+        console.log('Title: ' + argv.title)
     }
 })
 
@@ -46,4 +89,14 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
+
+// console.log(yargs.argv)
+
+// 
+// Challenge: Add an option to yargs
+// 
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
