630a2ec6e7f20abf10c0955c0fd5de38c67085f1
Ritz Punsalan <ritzpunsalan@gmail.com>
2020-01-14 00:24:14 -0800

    working on stuff

---
+++ Section 4 Files System and Command Line Args Notes app/18playground/18-json.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+
+// console.log(bookJSON)
+// // console.log(bookJSON.title)
+
+// const parsedData = JSON.parse(bookJSON)
+// console.log(parsedData.author)
+
+// fs.writeFileSync('18json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('18json.json')
+// // console.log(dataBuffer)
+// // console.log(dataBuffer.toString())
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+// 
+// Challenge: Work with JSON and the file system
+//
+// 1. Load and parse the JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4. Test your work by viewing data in the JSON file
