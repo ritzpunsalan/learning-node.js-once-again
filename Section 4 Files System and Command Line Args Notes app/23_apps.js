91744715b4c4eccb61dcc8f4e07d4a7d17971dbd
Ritz Punsalan <ritzpunsalan@gmail.com>
2020-01-23 00:58:07 -0800

    sick but 23 donezo slightly. needs a refix.

---
+++ Section 4 Files System and Command Line Args Notes app/23_apps.js
@@ -0,0 +1,78 @@
+const chalk = require('chalk');
+const yargs = require('yargs')
+const notes = require('./23notes.js')
+
+// 
+// Goal: Wire up list command
+// 
+// 1. Create and export listNotes from 23notes.js
+//  - "Your notes" using chalk
+//  - Print note title for each note
+// 2. Call listNotes from command handler
+// 3. Test your work!
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe:'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    // handler: function (argv) {
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    // handler: function (argv) {
+    handler(argv) {
+        // console.log('Removing the note')
+        notes.removeNote(argv.title)
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    // handler: function () {
+    handler() {
+        console.log('Reading the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'list a note',
+    // handler: function () {
+    handler() {
+        // console.log('listing the note')
+        notes.listNotes()
+    }
+})
+
+yargs.parse()
